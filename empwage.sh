1b3c4974031e2be906ef6a02e21ff9e14ecb1df8
unknown <mfjkd7795@gmail.com>
2021-06-03 00:39:30 -0700

    Added: Part Time Daily Wage

--- empwage.sh
+++ empwage.sh
@@ -7,24 +7,13 @@ empHr=0
 totalWage=0
 
 randomnumber=$((RANDOM%3))
-if [ $randomnumber -eq $isFullTime ]
-then 
-	echo "Employee is Present"
-elif [ $randomnumber -eq $isPartTime ]
+if [ $randomnumber -eq $isPartTime ]
 then 
 	echo "Employee is Part Time"
+	empHr=4
 else
 	echo "Absent"
+	empHr=0
 fi
-
-if [ $randomnumber -eq $isFullTime ]
-then
-        echo "Employee is Present"
-        empHr=8
-else
-        echo "Absent"
-        empHr=0
-fi
-
 totalWage=$(($wagePerHr*$empHr))
 echo "Daily Wage of Employee is:" $totalWage
