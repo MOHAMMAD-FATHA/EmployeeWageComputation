c768f0925de2eb9abcfca240cb8b868b0d9af0f3
unknown <mfjkd7795@gmail.com>
2021-06-03 00:22:18 -0700

    Added: uc1_present_or_absent

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,15 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 
+isFullTime=1
+isPartTime=2
 
+randomnumber=$((RANDOM%3))
+if [ $randomnumber -eq $isFullTime ]
+then 
+	echo "Employee is Present"
+elif [ $randomnumber -eq $isPartTime ]
+then 
+	echo "Employee is Part Time"
+else
+	echo "Absent"
+fi
